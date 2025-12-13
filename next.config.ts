9c25fd86c7631280d56351fbb2b2fda812b04405
swarajns <swarajns79@gmail.com>
2025-12-13 11:51:44 +0530

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['img.youtube.com'], // For YouTube thumbnails
-  },
-  
-  experimental: {
-    allowedDevOrigins: [
-      '192.0.0.2',
-      'localhost',
-      '127.0.0.1',
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
